29f851f511b0b0efecc7c5e82f3d6a6a4c8f4bf7
Boboy Infante <hm0ejd74@gmail.com>
2023-05-03 17:59:33 +0800

    score
    added player score and computer score

--- src/Gui.java
+++ src/Gui.java
@@ -12,6 +12,10 @@ public class Gui implements ActionListener{
 	private static JPanel panel;
 	private static Random random;
 	private static String pcmove;
+	private int computerscore = 0;
+	private int playerscore = 0;
+	private static JLabel labelplayer;
+	private static JLabel labelcomputer;
 	
 	public void Frame() {
 		
@@ -31,17 +35,25 @@ public class Gui implements ActionListener{
 		paper.setPreferredSize(new Dimension(100, 150));
 		scissor.setPreferredSize(new Dimension(100, 150));
 		
+		labelplayer = new JLabel("Player Score: " + labelplayer);
+		labelcomputer = new JLabel("Computer Score: " + labelcomputer);
+		
+		labelcomputer.setLocation(30, 30);
+		
 		JPanel panel = new JPanel();
+		panel.add(labelcomputer);
+		panel.add(labelplayer);
 		panel.add(rock);
 		panel.add(paper);
 		panel.add(scissor);
+		
 		
 		frame.getContentPane().add(panel);
 		
 		frame.pack();
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
-		frame.setSize(360, 200);
+		frame.setSize(360, 400);
 		
 		Random rand = new Random();
 		
@@ -57,29 +69,36 @@ public class Gui implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		
 		if(pcmove.equals("r") && e.getSource() == rock) {
 			JOptionPane.showMessageDialog(null, "TIE!");
-		}else if(pcmove.equals("r") && e.getSource() == paper) {
-			JOptionPane.showMessageDialog(null, "You Won!");
-		}else if(pcmove.equals("r") && e.getSource() == scissor) {
-			JOptionPane.showMessageDialog(null, "Computer Won!");
-		}else if(pcmove.equals("p") && e.getSource() == rock) {
-			JOptionPane.showMessageDialog(null, "Computer Won!");
 		}else if(pcmove.equals("p") && e.getSource() == paper) {
-			JOptionPane.showMessageDialog(null, "TIE");
-		}else if(pcmove.equals("p") && e.getSource() == scissor) {
-			JOptionPane.showMessageDialog(null, "You Won!");
+			JOptionPane.showMessageDialog(null, "TIE!");
 		}else if(pcmove.equals("s") && e.getSource() == scissor) {
-			JOptionPane.showMessageDialog(null, "You Won!");
+			JOptionPane.showMessageDialog(null, "TIE!");
+		}else if(pcmove.equals("r") && e.getSource() == scissor) {
+			JOptionPane.showMessageDialog(null, "Computer Won");
+			computerscore++;
+		}else if(pcmove.equals("r") && e.getSource() == paper) {
+			JOptionPane.showMessageDialog(null, "Player Won");
+			playerscore++;
 		}else if(pcmove.equals("s") && e.getSource() == paper) {
-			JOptionPane.showMessageDialog(null, "Computer Won!");
+			JOptionPane.showMessageDialog(null, "Computer Won");
+			computerscore++;		
 		}else if(pcmove.equals("s") && e.getSource() == rock) {
-			JOptionPane.showMessageDialog(null, "You Won!");
+			JOptionPane.showMessageDialog(null, "Player Won");
+			playerscore++;
+		}else if(pcmove.equals("p") && e.getSource() == rock) {
+			JOptionPane.showMessageDialog(null, "Computer Won");
+			computerscore++;
+		}else if(pcmove.equals("p") && e.getSource() == scissor) {
+			JOptionPane.showMessageDialog(null, "Player Won");
+			playerscore++;
+		
 		}
+		labelcomputer.setText("Computer: " + computerscore);
+		labelplayer.setText("Player: " + playerscore);
 	}
 		
-	}	
+}	
 	
 
